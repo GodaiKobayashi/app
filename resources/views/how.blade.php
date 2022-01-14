6b2e07b1b40fe8099c121860f4e159ed689b5e9a
GodaiKobayashi <ec2-user@ip-172-31-10-150.ap-northeast-1.compute.internal>
2022-01-14 12:55:12 +0900

    #

--- resources/views/how.blade.php
+++ resources/views/how.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>使い方</h1>
+<a href="/auth/twitter">ログイン</a>
+<a href="/auth/twitter/logout">ログアウト</a>/tweets.json?q=twitterapi
+
+<h1>使い方!</h1>
 <div class="img">
 <img src="/storage/how.jpg">
 </div>
--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,10 @@ Route::get('/how', 'ProfileController@how')->name('how');
 
 Route::get('/search', 'ProfileController@search')->name('search');
 
-Route::get('twitter', 'TwitterController@index');
+// ログインURL
+Route::get('auth/twitter', 'Auth\TwitterController@redirectToProvider');
+// コールバックURL
+Route::get('auth/twitter/callback', 'Auth\TwitterController@handleProviderCallback');
+// ログアウトURL
+Route::get('auth/twitter/logout', 'Auth\TwitterController@logout');
 
