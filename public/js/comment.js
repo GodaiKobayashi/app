a3cd6b98feef242985a3fbb2024c8d63397ea64a
GodaiKobayashi <ec2-user@ip-172-31-10-150.ap-northeast-1.compute.internal>
2022-01-16 11:26:23 +0900

    edit

--- public/js/comment.js
+++ public/js/comment.js
@@ -5,7 +5,7 @@ $(function() {
 
 function get_data() {
     $.ajax({
-        url: "result/ajax/",
+        url: "result/ajax",
         dataType: "json",
         success: data => {
             $("#comment-data")
